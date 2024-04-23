b2da3d71dba65ea300700b1b90cb38cff6ac497d
KristianMoellmann <kr_moellmann@hotmail.com>
2024-04-23 17:04:55 +0200

    Added k-fold cross validation

--- src/train_model.py
+++ src/train_model.py
@@ -9,6 +9,7 @@ from torch.utils.data import DataLoader, random_split
 from argparse import ArgumentParser
 from pathlib import Path
 from tqdm import tqdm
+from sklearn.model_selection import KFold
 # from src.models.model import ScaleNet
 
 
@@ -82,7 +83,7 @@ def train(model: nn.Module, train_loader: DataLoader, val_loader: DataLoader, op
 
     best_val_loss = float('inf')
 
-    for epoch in (pbar := tqdm(range(epochs))):
+    for epoch in (pbar := tqdm(range(epochs), leave=True)):
         pbar.set_description(f"Epoch {epoch+1}/{epochs}")
         train_loss = 0
         for image, target in train_loader:
@@ -142,22 +143,23 @@ def plot_predictions_scale9(model: nn.Module, test_loader: DataLoader, loss_func
     test_predictions = torch.round(torch.cat(test_predictions).cpu() * 8 + 1)
     test_targets = torch.cat(test_targets).cpu() * 8 + 1
 
-    # Plot histofram of test predicitons
-    fig, axes = plt.subplots(3, 3, figsize=(12, 8))
+    if not args.dont_plot:
+        # Plot histofram of test predicitons
+        fig, axes = plt.subplots(3, 3, figsize=(12, 8))
 
-    for i, ax in enumerate(axes.flatten()):
-        # Bar plot of predictions
-        ax.hist(test_predictions[test_targets == i+1], bins=9, alpha=0.5, label='Predictions', color='blue')
-        # plot mean of predictions
-        ax.axvline(test_predictions[test_targets == i+1].mean(), color='red', linestyle='dashed', linewidth=2)
-        ax.set_xlim(1, 10)
-        # ax.set_ylim(0, 30)
-        ax.xaxis.set_ticks(range(0, 11))
-        ax.set_title(f"True rating: {i+1}")
-    fig.suptitle(f"Predictions on test set, test loss: {test_loss:.2f}")
-    plt.tight_layout()
-    plt.show()
-
+        for i, ax in enumerate(axes.flatten()):
+            # Bar plot of predictions
+            ax.hist(test_predictions[test_targets == i+1], bins=9, alpha=0.5, label='Predictions', color='blue')
+            # plot mean of predictions
+            ax.axvline(test_predictions[test_targets == i+1].mean(), color='red', linestyle='dashed', linewidth=2)
+            ax.set_xlim(1, 10)
+            # ax.set_ylim(0, 30)
+            ax.xaxis.set_ticks(range(0, 11))
+            ax.set_title(f"True rating: {i+1}")
+        fig.suptitle(f"Predictions on test set, test loss: {test_loss:.2f}")
+        plt.tight_layout()
+        plt.show()
+    return test_loss
 
 def plot_predictions_elo(model: nn.Module, test_loader: DataLoader, loss_func: callable, device: str):
     # Plot predictions on test set
@@ -175,23 +177,26 @@ def plot_predictions_elo(model: nn.Module, test_loader: DataLoader, loss_func: c
             test_targets.append(target)
             loss = loss_func(pred, target)
             test_loss += loss.item()/len(test_loader)
-
+    
+    print(f"Test loss: {test_loss:.3f}")
     test_predictions = torch.round(torch.cat(test_predictions).cpu() * (r_max - r_min) + r_min)
     test_targets = torch.cat(test_targets).cpu() * (r_max - r_min) + r_min
 
-    # Plot histofram of test predicitons and targets
-    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
-    for ax, data, title in zip(axes, [test_predictions, test_targets], ['Predictions', 'Targets']):
-        ax.hist(data, bins=100, alpha=0.5, label=title, color='blue')
-        ax.axvline(data.mean(), color='red', linestyle='dashed', linewidth=2)
-        ax.set_xlim(r_min-5, r_max+5)
-        # ax.set_ylim(0, 30)
-        # ax.xaxis.set_ticks(range(int(r_min), int(r_max)+1))
-        ax.set_title(title)
-    fig.suptitle(f"Predictions on test set, test loss: {test_loss:.2f}")
-    plt.tight_layout()
-    plt.savefig(f"reports/figures/training/{args.name}_{args.scoring}_{args.score_type}_{model.name}_predictions.pdf")
-    plt.show()
+    if not args.dont_plot:
+        # Plot histofram of test predicitons and targets
+        fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+        for ax, data, title in zip(axes, [test_predictions, test_targets], ['Predictions', 'Targets']):
+            ax.hist(data, bins=100, alpha=0.5, label=title, color='blue')
+            ax.axvline(data.mean(), color='red', linestyle='dashed', linewidth=2)
+            ax.set_xlim(r_min-5, r_max+5)
+            # ax.set_ylim(0, 30)
+            # ax.xaxis.set_ticks(range(int(r_min), int(r_max)+1))
+            ax.set_title(title)
+        fig.suptitle(f"Predictions on test set, test loss: {test_loss:.2f}")
+        plt.tight_layout()
+        plt.savefig(f"reports/figures/training/{args.name}_{args.scoring}_{args.score_type}_{model.name}_predictions.pdf")
+        plt.show()
+    return test_loss
 
 
 if __name__=='__main__':
@@ -206,26 +211,12 @@ if __name__=='__main__':
     parser.add_argument('--embed_now', action='store_true', help="Embed the images now")
     parser.add_argument('--seed', default=42, type=int, help="Seed to use for reproducibility")
     parser.add_argument('--score_type', default='original', choices=['original', 'logic', 'clip'], help="Decide which score type to use")
+    parser.add_argument('--dont_plot', action='store_true', help="Plot the predictions on the test set")
     args = parser.parse_args()
 
     # Load the feautre extractor and the preprocess function
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    if args.embed_now:
-        clip_model, preprocess = clip.load(args.model, device=device)
-
-        # Create the model
-        feature_extractor = clip_model.visual
-        for param in feature_extractor.parameters():
-            param.requires_grad = False
-        feature_extractor.float()
-        feature_extractor.eval()
-
-        model = ScaleNetWithClip(feature_extractor).to(device)
-    
-    else:
-        args.folder = args.folder.replace('processed', 'embedded')
-        model = ScaleNetV2().to(device)
 
     # Load the data
     if args.score_type == 'original':
@@ -243,58 +234,98 @@ if __name__=='__main__':
     with open(scoring_path, 'r') as f:
         labels = json.load(f)
 
-    # Initialise things which must be defined according to the scoring method
-    data = None
-    loss_func = None
-    optimizer = None
-
-    if args.scoring == 'elo':
-        if args.embed_now:
-            raise ValueError("Elo scoring is not supported with 'embed_now' option. Please embed the images first using src/data/embed_dataset.py.")
-        else:
-            data = EmbeddedEloDataset(args.folder, labels)
-            r_min, r_max = data.r_min, data.r_max
-    else:
-        if args.embed_now:
-            preprocess = clip_model.transform
-            data = ScaleDataset(args.folder, labels, preprocess)
-        else:
-            data = EmbeddedScaleDataset(args.folder, labels)
-    loss_func = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    labels_train_val, labels_test = random_split(labels, [int(0.90*len(labels)), int(0.10*len(labels))])
     
-    # Split the data
-    train_ratio, val_ratio, test_ratio = 0.7, 0.15, 0.15
-    n = len(data)
-
-    train_data, val_data, test_data = random_split(data, [int(train_ratio*n), int(val_ratio*n), int(test_ratio*n)])
+    kf = KFold(n_splits=9, shuffle=True, random_state=args.seed)
+    test_losses = []
     
-    # Create data loader
-    train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True)
-    val_loader = DataLoader(val_data, batch_size=args.batch_size, shuffle=False)
-    test_loader = DataLoader(test_data, batch_size=args.batch_size, shuffle=False)
+    # kf_iterator = tqdm(kf.split(labels_train_val), total=kf.get_n_splits(), leave=False)
+    for i, (train_index, val_index) in enumerate(kf.split(labels_train_val)):
+        print(f"Fold {i+1}/9")
+        labels_train_key = [[*labels_train_val.dataset.keys()][i] for i in train_index]
+        labels_train = {k: labels_train_val.dataset[k] for k in labels_train_key}
+        
+        labels_val_key = [[*labels_train_val.dataset.keys()][i] for i in val_index]
+        labels_val = {k: labels_train_val.dataset[k] for k in labels_val_key}
 
-    # Train the model
-    model_save_path = f'models/{args.scoring}/{os.path.basename(args.folder)}'
-    os.makedirs(model_save_path, exist_ok=True)
+        if args.embed_now:
+            clip_model, preprocess = clip.load(args.model, device=device)
 
-    train_losses, val_losses = train(model, train_loader, val_loader, optimizer, loss_func, args.epochs, device, args.scoring)
+            # Create the model
+            feature_extractor = clip_model.visual
+            for param in feature_extractor.parameters():
+                param.requires_grad = False
+            feature_extractor.float()
+            feature_extractor.eval()
+            model = ScaleNetWithClip(feature_extractor).to(device)
+        else:
+            args.folder = args.folder.replace('processed', 'embedded')
+            model = ScaleNetV2().to(device)
+        
+        # Initialise things which must be defined according to the scoring method
+        data = None
+        loss_func = None
+        optimizer = None
 
-    # Plot the losses
-    plt.plot(train_losses, label='Training loss')
-    plt.plot(val_losses, label='Validation loss')
-    plt.legend()
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.title(f'Training and validation losses. Best val loss: {min(val_losses):.3f}')
-    plt.savefig(f"reports/figures/training/{args.name}_{args.scoring}_{args.score_type}_{model.name}_loss.pdf")
-    plt.show()
+        if args.scoring == 'elo':
+            if args.embed_now:
+                raise ValueError("Elo scoring is not supported with 'embed_now' option. Please embed the images first using src/data/embed_dataset.py.")
+            else:
+                train_data = EmbeddedEloDataset(args.folder, labels_train)
+                val_data = EmbeddedEloDataset(args.folder, labels_val)
+                test_data = EmbeddedEloDataset(args.folder, labels_test.dataset)
+                r_min, r_max = train_data.r_min, train_data.r_max
+        else:
+            if args.embed_now:
+                preprocess = clip_model.transform
+                train_data = ScaleDataset(args.folder, labels_train, preprocess)
+                val_data = ScaleDataset(args.folder, labels_val, preprocess)
+                test_data = ScaleDataset(args.folder, labels_test.dataset, preprocess)
+            else:
+                train_data = EmbeddedScaleDataset(args.folder, labels_train)
+                val_data = EmbeddedScaleDataset(args.folder, labels_val)
+                test_data = EmbeddedScaleDataset(args.folder, labels_test.dataset)
+                
+        loss_func = nn.MSELoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+        
+        # Split the data
+        # train_ratio, val_ratio, test_ratio = 0.7, 0.15, 0.15
+        # n = len(data)
 
-    # Load the best model
-    model.load_state_dict(torch.load(f'models/{args.scoring}/{os.path.basename(args.folder)}/{args.name}.pt'))
+        # train_data, val_data, test_data = random_split(data, [int(train_ratio*n), int(val_ratio*n), int(test_ratio*n)])
+        
+        # Create data loader
+        train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True)
+        val_loader = DataLoader(val_data, batch_size=args.batch_size, shuffle=False)
+        test_loader = DataLoader(test_data, batch_size=args.batch_size, shuffle=False)
 
-    if args.scoring == 'scale_9':
-        plot_predictions_scale9(model, test_loader, loss_func, device)
-    else:
-        plot_predictions_elo(model, test_loader, loss_func, device)
+        # Train the model
+        model_save_path = f'models/{args.scoring}/{os.path.basename(args.folder)}'
+        os.makedirs(model_save_path, exist_ok=True)
+
+        train_losses, val_losses = train(model, train_loader, val_loader, optimizer, loss_func, args.epochs, device, args.scoring)
+
+        if not args.dont_plot:
+            # Plot the losses
+            plt.plot(train_losses, label='Training loss')
+            plt.plot(val_losses, label='Validation loss')
+            plt.legend()
+            plt.xlabel('Epoch')
+            plt.ylabel('Loss')
+            plt.title(f'Training and validation losses. Best val loss: {min(val_losses):.3f}')
+            plt.savefig(f"reports/figures/training/{args.name}_{args.scoring}_{args.score_type}_{model.name}_loss.pdf")
+            plt.show()
+
+        # Load the best model
+        model.load_state_dict(torch.load(f'models/{args.scoring}/{os.path.basename(args.folder)}/{args.name}.pt'))
+
+        if args.scoring == 'scale_9':
+            test_loss = plot_predictions_scale9(model, test_loader, loss_func, device)
+        else:
+            test_loss = plot_predictions_elo(model, test_loader, loss_func, device)
+        
+        test_losses.append(test_loss)
+        
+    print(f"Average test loss: {sum(test_losses)/len(test_losses):.3f}")
     
