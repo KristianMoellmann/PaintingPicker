35a996d2c2f2a09cb1c8ab35a13ff3f6f9a28d7e
KrisMarboe <kristoffermarboe@gmail.com>
2024-04-26 18:12:22 +0200

    sfsfs

--- src/visualizations/scale.py
+++ src/visualizations/scale.py
@@ -6,25 +6,33 @@ from argparse import ArgumentParser
 from pathlib import Path
 
 
-def plot_scale_count(scores: dict, scale: int):
+def plot_scale_count(scores_dict: dict, scale: int):
     """
     Plot the count of each scale in the scores dictionary
 
     Args:
         scores (dict): The scores dictionary {"img_id": score}
     """
-    scales = np.arange(1, scale + 1)
-    counts = np.zeros(scale)
-    for score in scores.values():
-        counts[score - 1] += 1
+    fig, ax = plt.subplots(1, 1, figsize=(5, 3.5))
+    plt.grid(True, linestyle='--', alpha=0.5, zorder=0)
+    offset = -0.2
 
-    plt.figure(figsize=(10, 5))
-    plt.bar(scales, counts)
-    plt.xlabel('Scale')
-    plt.ylabel('Count')
-    plt.title('Count of each scale')
+    colors = ['royalblue', 'mediumseagreen', 'indianred']
+    
+    for i, (name, scores) in enumerate(scores_dict.items()):
+        scales = np.arange(1, scale + 1)
+        counts = np.zeros(scale)
+        for score in scores.values():
+            counts[score - 1] += 1
+        ax.bar(scales + offset, counts, label=name.capitalize(), width=0.2, zorder=5, alpha=0.8, color=colors[i])
+        offset += 0.2
+    
+    ax.set_xticks(scales)
+    ax.set_xlabel('Scale')
+    ax.set_ylabel('Count')
+    ax.legend()
     plt.tight_layout()
-    plt.show()
+    plt.savefig(f"reports/figures/scale/scale_count.pdf")
 
 def plot_history(scores: dict, scale: int):
     """
@@ -57,18 +65,21 @@ def plot_history(scores: dict, scale: int):
 
 if __name__=='__main__':
     parser = ArgumentParser()
-    parser.add_argument('name', type=str, help="Name of the user")
-    parser.add_argument('--folder', default='data/fewer_imgs', type=str, help="Folder containing images")
+    parser.add_argument('names', type=str, nargs="*", help="Names of the users")
+    parser.add_argument('--dataset', default='full', type=str, help="Folder containing images")
     parser.add_argument('--scale', type=int, default=9, help="Number of scales")
     args = parser.parse_args()
 
-    scores_file = Path(f'scores/{os.path.basename(args.folder)}/scale_{args.scale}/{args.name}.json')
+    scores = {}
+    for name in args.names:
+        scores_file = Path(f'scores/{args.dataset}/scale_{args.scale}/{name}.json')
 
-    if not scores_file.exists():
-        raise FileNotFoundError(f"Scores file not found: {scores_file}")
-    
-    with open(scores_file, 'r') as f:
-        scores = json.load(f)
+        if not scores_file.exists():
+            raise FileNotFoundError(f"Scores file not found: {scores_file}")
+        
+        with open(scores_file, 'r') as f:
+            score = json.load(f)
+        scores[name] = score
     
     plot_scale_count(scores, args.scale)
-    plot_history(scores, args.scale)
+    # plot_history(scores, args.scale)
